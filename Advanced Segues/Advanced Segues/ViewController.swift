1b915ec276c72f29daaa91fa5f488efa4b6c722b
bits2learn <nishant.t6419@gmail.com>
2015-06-17 18:30:36 -0700

    Adding Advanced Segues program

---
+++ Advanced Segues/Advanced Segues/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Advanced Segues
+//
+//  Created by Nishant Chapa on 6/17/15.
+//  Copyright (c) 2015 BitsFriends. All rights reserved.
+//
+
+import UIKit
+
+var counter:Int = 0
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println(counter)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

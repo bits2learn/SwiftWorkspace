ccc24570fe187b29d1a72cba0fa8f300d10dfde8
bits2learn <nishant.t6419@gmail.com>
2015-06-18 19:44:02 -0700

    Adding Audio Player project

--- Audio/Audio/ViewController.swift
+++ Audio/Audio/ViewController.swift
@@ -12,29 +12,33 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player:AVAudioPlayer = AVAudioPlayer()
+    var error : NSError? = nil
+    
+     var isPlayButton = true
     
     @IBAction func play(sender: AnyObject) {
         
-        var audioPath = NSBundle.mainBundle().pathForResource("Sooraj", ofType: "mp3")!
+        player.play()
+        isPlayButton = true
+        playPauseButton()
+       
+    }
+    
+    
+    @IBAction func stop(sender: AnyObject) {
         
-        var error : NSError? = nil
-        
-        player = AVAudioPlayer(contentsOfURL: NSURL(string: audioPath), error: &error)
-        
-        if error == nil {
-            
-            player.play()
-            
-        } else {
-            
-            println(error)
-        }
+        player.stop()
+        player.currentTime = 0
+        isPlayButton = false
+        playPauseButton()
     }
     
     
     @IBAction func pause(sender: AnyObject) {
         
         player.pause()
+        isPlayButton = false
+        playPauseButton()
     }
     
     
@@ -45,10 +49,39 @@ class ViewController: UIViewController {
     
     
     @IBOutlet var sliderValue: UISlider!
+    
+ 
+    @IBOutlet var toolbar: UIToolbar!
+    
+    func playPauseButton(){
+        
+        var new_button = UIBarButtonItem()
+        
+        if isPlayButton {
+            
+            new_button = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Pause, target: self, action: "pause:")
+            
+        } else {
+            
+            new_button = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Play, target: self, action: "play:")
+            
+        }
+        
+        var tb_items = toolbar.items
+        tb_items?.removeAtIndex(0)
+        tb_items?.insert(new_button, atIndex: 0)
+        toolbar.setItems(tb_items, animated: true)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var audioPath = NSBundle.mainBundle().pathForResource("EyeOfTheTiger", ofType: "mp3")!
+        
+        player = AVAudioPlayer(contentsOfURL: NSURL(string: audioPath), error: &error)
+
     }
 
     override func didReceiveMemoryWarning() {
